c3514fedee5a5210606ae1193dd52af86ef66e25
Roman Kolesnev <rvkolesnev@gmail.com>
2018-12-01 21:43:02 +0300

    Better Queue protocol definition

--- lib/peque/queue.ex
+++ lib/peque/queue.ex
@@ -2,15 +2,15 @@ defprotocol Peque.Queue do
   @type message :: term()
   @type ack_id :: pos_integer()
 
-  @spec add(t(), message()) :: {:ok, t()} | {:error, t()}
+  @spec add(t(), message()) :: {:ok, t()} | {:error, any(), t()}
   def add(queue, message)
 
-  @spec get(t()) :: {:ok, t(), ack_id(), message()} | {:empty, t()} | {:error, t()}
+  @spec get(t()) :: {:ok, t(), ack_id(), message()} | {:empty, t()} | {:error, any(), t()}
   def get(queue)
 
-  @spec ack(t(), ack_id()) :: {:ok, t()} | {:not_found, t()} | {:error, t()}
+  @spec ack(t(), ack_id()) :: {:ok, t()} | {:not_found, t()} | {:error, any(), t()}
   def ack(queue, ack_id)
 
-  @spec reject(t(), ack_id()) :: {:ok, t()} | {:not_found, t()} | {:error, t()}
+  @spec reject(t(), ack_id()) :: {:ok, t()} | {:not_found, t()} | {:error, any(), t()}
   def reject(queue, ack_id)
 end
